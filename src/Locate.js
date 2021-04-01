f0367707b8e695122bda22d22b146a5faf05f280
Amreith <72003702+Amreithvenkatesh@users.noreply.github.com>
2021-04-01 10:04:09 +0530

    Update Locate.js

--- src/Locate.js
+++ src/Locate.js
@@ -1,37 +1,128 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
-import './Locate.css';
+import React, { useState, useRef } from "react";
+import useSwr from "swr";
+import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
+import useSupercluster from "use-supercluster";
+import "./Locate.css";
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Button } from "@material-ui/core";
 
+const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 export default function Locate() {
-    const [viewport,setViewport] = useState({
-        latitude :28.704060,
-        longitude:77.102493,
-        zoom:10,
-        width: "100%",
-        height:"500px"
-    })
-    return (
-        <div className="locate">
-            <div className="locate__header">
-                <h3>Medicine available in:</h3>
-            </div>
-            <div className="locate__map">
-                <ReactMapGL 
-                {...viewport}
-                mapStyle="mapbox://styles/amreith/ckms1h28t2miv17p7ai6tcsip"
-                mapboxApiAccessToken="pk.eyJ1IjoiYW1yZWl0aCIsImEiOiJja21zMG55bnowZDlsMm9zMWN4NzlqMXR0In0.JxDiRT_kLg9Z3e_5RPJTUw"
-                onViewportChange={viewport =>{
-                    setViewport(viewport);
-                }}
-                >
+  const [viewport, setViewport] = useState({
+    latitude: 18.820089,
+    longitude: 76.770653,
+    width: "80vw",
+    height: "80vh",
+    zoom: 4
+  });
+  const mapRef = useRef();
 
-                </ReactMapGL>
-            </div>
-            
-        </div>
-    )
+  const url =
+    "http://localhost:8000/updateMed/";
+  const { data, error } = useSwr(url, { fetcher });
+  const kendras = data && !error ? data.slice(0, 8000) : [];
+  const points = kendras.map(kendra => ({
+    type: "Feature",
+    properties: { cluster: false, kendraId: kendra.id, kendraDistrict: kendra.District },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(kendra.Longitude),
+        parseFloat(kendra.Latitude)
+      ]
+    }
+  }));
+
+  const bounds = mapRef.current
+    ? mapRef.current
+        .getMap()
+        .getBounds()
+        .toArray()
+        .flat()
+    : null;
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom: viewport.zoom,
+    options: { radius: 75, maxZoom: 20 }
+  });
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        maxZoom={20}
+        mapStyle="mapbox://styles/amreith/ckmxqtvno12wp17lq8593vrao"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={newViewport => {
+          setViewport({ ...newViewport });
+        }}
+        ref={mapRef}
+      >
+        {clusters.map(cluster => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={`cluster-${cluster.id}`}
+                latitude={latitude}
+                longitude={longitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 20}px`,
+                    height: `${10 + (pointCount / points.length) * 20}px`
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+
+                    setViewport({
+                      ...viewport,
+                      latitude,
+                      longitude,
+                      zoom: expansionZoom,
+                      transitionInterpolator: new FlyToInterpolator({
+                        speed: 2
+                      }),
+                      transitionDuration: "auto"
+                    });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker
+              key={`crime-${cluster.properties.kendraId}`}
+              latitude={latitude}
+              longitude={longitude}
+            >
+              <Button>
+                <LocationOnIcon />
+              </Button>
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
+    </div>
+  );
 }
+
+
 
 
 
