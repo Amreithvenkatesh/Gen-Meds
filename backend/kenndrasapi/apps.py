cb08b4191c6ddce6a7360783f31b15bd249e716c
Amreith <72003702+Amreithvenkatesh@users.noreply.github.com>
2021-07-12 23:17:09 +0530

    Delete backend directory

--- backend/kenndrasapi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KendrasapiConfig(AppConfig):
-    name = 'kendrasapi'
--- backend/kenndrasapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import genmeds
-
-class kendrasSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=genmeds
-        fields='__all__'
